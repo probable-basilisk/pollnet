d2ca60e116e2e98fe579f2c81951401bbe4cea7a
probable-basilisk <basilisk@mtknn.com>
2023-03-26 16:52:44 -0400

    forgot this

--- src/context.rs
+++ src/context.rs
@@ -1,10 +1,4 @@
-use std::collections::HashMap;
-use std::sync::RwLock;
-use std::sync::Arc;
-use std::thread;
 use std::net::SocketAddr;
-use std::io::Error as IoError;
-use std::path::Path;
 use slotmap::{HopSlotMap, Key};
 use log::{error, warn, info};
 use tokio::net::{TcpListener, TcpStream};
@@ -12,10 +6,10 @@ use tokio::runtime;
 use tokio::io::AsyncWriteExt;
 use tokio_tungstenite::{connect_async, accept_async};
 use futures::executor::block_on;
+use std::thread;
 use futures_util::{SinkExt, StreamExt, future};
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response};
-use hyper_staticfile::Static;
+
+mod httpfuncs;
 
 slotmap::new_key_type! {
   pub struct SocketHandle;
@@ -204,57 +198,6 @@ async fn accept_tcp(mut tcp_stream: TcpStream, addr: SocketAddr, outer_tx: Optio
     tcp_stream.shutdown().await.unwrap_or_default(); // if this errors we don't care
 }
 
-async fn handle_http_request<B>(req: Request<B>, static_: Option<Static>, virtual_files: Arc<RwLock<HashMap<String, Vec<u8>>>>) -> Result<Response<Body>, IoError> {
-    {
-        // Do we need like... more headers???
-        let vfiles = virtual_files.read().expect("RwLock poisoned");
-        if let Some(file_data) = vfiles.get(req.uri().path()) {
-            return Response::builder()
-                    .status(http::StatusCode::OK)
-                    .body(Body::from(file_data.clone()))
-                    .map_err(|_| IoError::new(std::io::ErrorKind::Other, "Rust errors are a pain"))
-        }
-    }
-
-    match static_ {
-        Some(static_) => static_.clone().serve(req).await,
-        None => {
-            Response::builder().status(http::StatusCode::NOT_FOUND).body(Body::empty()).map_err(|_| IoError::new(std::io::ErrorKind::Other, "Rust errors are a pain"))
-        }
-    }
-}
-
-async fn _handle_http_response(resp: reqwest::Result<reqwest::Response>, body_only: bool, dest: std::sync::mpsc::Sender<SocketMessage>) {
-    let resp = match resp {
-        Ok(resp) => resp,
-        Err(err) => {
-            error!("HTTP failed: {}", err);
-            dest.send(SocketMessage::Error(err.to_string())).expect("TX error sending error!");
-            return;
-        }
-    };
-    if !body_only {
-        let statuscode = resp.status().to_string();
-        dest.send(SocketMessage::BinaryMessage(statuscode.into())).expect("TX error on http status");
-        let mut headers = String::new();
-        for (key, value) in resp.headers().iter() {
-            headers.push_str(&key.to_string());
-            headers.push_str(":");
-            headers.push_str(value.to_str().unwrap_or("MALFORMED"));
-            headers.push_str(";\n");
-        }
-        dest.send(SocketMessage::BinaryMessage(headers.into())).expect("TX error on http headers");
-    };
-    match resp.bytes().await {
-        Ok(body) => {
-            dest.send(SocketMessage::BinaryMessage(body.to_vec())).expect("TX error on http body");
-        },
-        Err(body_err) => {
-            dest.send(SocketMessage::Error(body_err.to_string())).expect("TX error on http body error");
-        }
-    };
-}
-
 impl PollnetContext {
     pub fn new() -> PollnetContext {
         match env_logger::try_init() {
@@ -296,82 +239,6 @@ impl PollnetContext {
         }
     }
 
-    pub fn serve_http(&mut self, bind_addr: String, serve_dir: Option<String>) -> SocketHandle {
-        let (tx_to_sock, mut rx_to_sock) = tokio::sync::mpsc::channel(100);
-        let (tx_from_sock, rx_from_sock) = std::sync::mpsc::channel();
-
-        // Spawn a future onto the runtime
-        self.rt_handle.spawn(async move {
-            info!("HTTP server spawned");
-            let addr = bind_addr.parse();
-            if let Err(_) = addr {
-                error!("Invalid TCP address: {}", bind_addr);
-                tx_from_sock.send(SocketMessage::Error("Invalid TCP address".to_string())).unwrap_or_default();
-                return;
-            }
-            let addr = addr.unwrap();
-
-            let static_ = match serve_dir {
-                Some(path_string) => Some(Static::new(Path::new(&path_string))),
-                None => None
-            };
-
-            let virtual_files: HashMap<String, Vec<u8>> = HashMap::new();
-            let virtual_files = Arc::new(RwLock::new(virtual_files));
-            let virtual_files_two_the_clone_wars = virtual_files.clone();
-
-            let make_service = make_service_fn(|_| {
-                // Rust demands all these clones for reasons I don't fully understand
-                // I definitely feel so much safer though!
-                let static_ = static_.clone();
-                let virtual_files = virtual_files.clone();
-                future::ok::<_, hyper::Error>(service_fn(move |req| handle_http_request(req, static_.clone(), virtual_files.clone())))
-            });
-
-            let server = hyper::Server::try_bind(&addr);
-            if let Err(bind_err) = server {
-                error!("Couldn't bind {}: {}", bind_addr, bind_err);
-                tx_from_sock.send(SocketMessage::Error(bind_err.to_string())).unwrap_or_default();
-                return;
-            }
-            let server = server.unwrap().serve(make_service);
-            let graceful = server.with_graceful_shutdown(async move {
-                let virtual_files = virtual_files_two_the_clone_wars.clone();
-                loop {
-                    match rx_to_sock.recv().await {
-                        Some(SocketMessage::Disconnect) | Some(SocketMessage::Error(_)) | None => {
-                            break
-                        },
-                        Some(SocketMessage::FileAdd(filename, filedata)) => {
-                            let mut vfiles = virtual_files.write().expect("Lock is poisoned");
-                            vfiles.insert(filename, filedata);
-                        },
-                        Some(SocketMessage::FileRemove(filename)) => {
-                            let mut vfiles = virtual_files.write().expect("Lock is poisoned");
-                            vfiles.remove(&filename);
-                        },
-                        _ => {} // ignore sends?
-                    }
-                }
-                info!("HTTP server trying to gracefully exit?");
-            });
-            info!("HTTP server running on http://{}/", addr);
-            if let Err(err) = graceful.await {
-                tx_from_sock.send(SocketMessage::Error(err.to_string())).unwrap_or_default(); // don't care at this point
-            }
-            info!("HTTP server stopped.");
-        });
-
-        let socket = Box::new(PollnetSocket{
-            tx: tx_to_sock,
-            rx: rx_from_sock,
-            status: SocketStatus::OPENING,
-            message: None,
-            error: None,
-            last_client_handle: SocketHandle::null()
-        });
-        self.sockets.insert(socket)
-    }
 
     pub fn listen_ws(&mut self, addr: String) -> SocketHandle {
         let (tx_to_sock, mut rx_to_sock) = tokio::sync::mpsc::channel(100);
@@ -586,85 +453,6 @@ impl PollnetContext {
                 Err(err) => {
                     error!("TCP client connection error: {}", err);
                     tx_from_sock.send(SocketMessage::Error(err.to_string())).expect("TX error on connection error");
-                }
-            }
-        });
-
-        let socket = Box::new(PollnetSocket{
-            tx: tx_to_sock,
-            rx: rx_from_sock,
-            status: SocketStatus::OPENING,
-            message: None,
-            error: None,
-            last_client_handle: SocketHandle::null()
-        });
-        self.sockets.insert(socket)
-    }
-
-    async fn _handle_get(url: String, body_only: bool, dest: std::sync::mpsc::Sender<SocketMessage>) {
-        info!("HTTP GET: {}", url);
-        _handle_http_response(reqwest::get(&url).await, body_only, dest).await;
-    }
-
-    pub fn open_http_get_simple(&mut self, url: String, body_only: bool) -> SocketHandle {
-        let (tx_to_sock, mut rx_to_sock) = tokio::sync::mpsc::channel(100);
-        let (tx_from_sock, rx_from_sock) = std::sync::mpsc::channel();
-
-        self.rt_handle.spawn(async move {
-            let get_handler = PollnetContext::_handle_get(url, body_only, tx_from_sock);
-            tokio::pin!(get_handler);
-            loop {
-                tokio::select! {
-                    _ = &mut get_handler => break,
-                    from_c_message = rx_to_sock.recv() => {
-                        match from_c_message {
-                            Some(SocketMessage::Disconnect) => break,
-                            _ => ()
-                        }
-                    },
-                }
-            }
-        });
-
-        let socket = Box::new(PollnetSocket{
-            tx: tx_to_sock,
-            rx: rx_from_sock,
-            status: SocketStatus::OPENING,
-            message: None,
-            error: None,
-            last_client_handle: SocketHandle::null()
-        });
-        self.sockets.insert(socket)
-    }
-
-    async fn _handle_post(url: String, ret_body_only: bool, content_type: String, body: Vec<u8>, dest: std::sync::mpsc::Sender<SocketMessage>) {
-        info!("HTTP POST: {} (w/ {})", url, content_type);
-        let client = reqwest::Client::new();
-        let resp = client
-            .post(&url)
-            .header(reqwest::header::CONTENT_TYPE, content_type)
-            .body(body)
-            .send().await;
-        _handle_http_response(resp, ret_body_only, dest).await;
-    }
-
-    pub fn open_http_post_simple(&mut self, url: String, ret_body_only: bool, content_type: String, body: Vec<u8>) -> SocketHandle {
-        let (tx_to_sock, mut rx_to_sock) = tokio::sync::mpsc::channel(100);
-        let (tx_from_sock, rx_from_sock) = std::sync::mpsc::channel();
-
-        self.rt_handle.spawn(async move {
-            let post_handler = PollnetContext::_handle_post(
-                url, ret_body_only, content_type, body, tx_from_sock);
-            tokio::pin!(post_handler);
-            loop {
-                tokio::select! {
-                    _ = &mut post_handler => break,
-                    from_c_message = rx_to_sock.recv() => {
-                        match from_c_message {
-                            Some(SocketMessage::Disconnect) => break,
-                            _ => ()
-                        }
-                    },
                 }
             }
         });
