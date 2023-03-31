af76b084c75a027446e5369a6820c104c8b281e7
probable-basilisk <basilisk@mtknn.com>
2023-03-31 01:07:12 -0400

    anyhow experiment

--- src/context/wsfuncs.rs
+++ src/context/wsfuncs.rs
@@ -4,18 +4,23 @@ use tokio_tungstenite::WebSocketStream;
 use tungstenite::protocol::Message;
 
 use super::*;
+use anyhow::anyhow;
 
-async fn websocket_poll_loop<S>(
-    mut ws_stream: WebSocketStream<S>,
+// SendErrors are ironically not themselves sync so this
+// function just turns any send error into an anyhow string error
+fn eh<T>(r: Result<(), std::sync::mpsc::SendError<T>>) -> anyhow::Result<()> {
+    r.map_err(|_| anyhow!("Channel TX Error"))
+}
+
+async fn websocket_poll_loop_inner<S>(
+    ws_stream: &mut WebSocketStream<S>,
     tx_from_sock: std::sync::mpsc::Sender<PollnetMessage>,
     mut rx_to_sock: tokio::sync::mpsc::Receiver<PollnetMessage>,
-) where
+) -> anyhow::Result<()>
+where
     S: AsyncRead + AsyncWrite + Unpin,
 {
-    if tx_from_sock.send(PollnetMessage::Connect).is_err() {
-        error!("Channel closed before WS finished opening.");
-        return;
-    }
+    eh(tx_from_sock.send(PollnetMessage::Connect))?;
     loop {
         tokio::select! {
             from_c_message = rx_to_sock.recv() => {
@@ -23,24 +28,24 @@ async fn websocket_poll_loop<S>(
                     Some(PollnetMessage::Text(msg)) => {
                         if let Err(e) = ws_stream.send(Message::Text(msg)).await {
                             debug!("WS send error.");
-                            tx_from_sock.send(PollnetMessage::Error(e.to_string())).unwrap_or_default();
-                            break;
+                            eh(tx_from_sock.send(PollnetMessage::Error(e.to_string())))?;
+                            return Ok(());
                         };
                     },
                     Some(PollnetMessage::Binary(msg)) => {
                         if let Err(e) = ws_stream.send(Message::Binary(msg)).await {
                             debug!("WS send error.");
-                            tx_from_sock.send(PollnetMessage::Error(e.to_string())).unwrap_or_default();
-                            break;
+                            eh(tx_from_sock.send(PollnetMessage::Error(e.to_string())))?;
+                            return Ok(());
                         };
                     },
                     Some(PollnetMessage::Disconnect) => {
                         debug!("Client-side disconnect.");
-                        break
+                        return Ok(());
                     },
                     None => {
                         warn!("Channel closed w/o disconnect message.");
-                        break
+                        return Ok(());
                     },
                     _ => {
                         error!("Invalid message to WS!");
@@ -50,28 +55,72 @@ async fn websocket_poll_loop<S>(
             from_sock_message = ws_stream.next() => {
                 match from_sock_message {
                     Some(Ok(msg)) => {
-                        if tx_from_sock.send(PollnetMessage::Binary(msg.into_data())).is_err() {
-                            error!("Channel closed in some bad way.");
-                            break;
-                        }
+                        eh(tx_from_sock.send(PollnetMessage::Binary(msg.into_data())))?;
                     },
                     Some(Err(msg)) => {
-                        debug!("Socket error.");
-                        tx_from_sock.send(PollnetMessage::Error(msg.to_string())).unwrap_or_default();
-                        break;
+                        info!("WS error.");
+                        eh(tx_from_sock.send(PollnetMessage::Error(msg.to_string())))?;
+                        return Ok(());
                     },
                     None => {
-                        debug!("Socket disconnect.");
-                        tx_from_sock.send(PollnetMessage::Disconnect).unwrap_or_default();
-                        break;
+                        info!("WS disconnect.");
+                        eh(tx_from_sock.send(PollnetMessage::Disconnect))?;
+                        return Ok(());
                     }
                 }
             },
         };
     }
+}
+
+async fn websocket_poll_loop<S>(
+    mut ws_stream: WebSocketStream<S>,
+    tx_from_sock: std::sync::mpsc::Sender<PollnetMessage>,
+    rx_to_sock: tokio::sync::mpsc::Receiver<PollnetMessage>,
+) where
+    S: AsyncRead + AsyncWrite + Unpin,
+{
+    if let Err(e) = websocket_poll_loop_inner(&mut ws_stream, tx_from_sock, rx_to_sock).await {
+        error!("Unexpected WS loop termination: {:?}", e);
+    }
     info!("Closing websocket!");
-    // if this errors we don't care
+    // At this point errors don't matter
     ws_stream.close(None).await.unwrap_or_default();
+}
+
+async fn accept_ws_inner<S>(
+    mut stream: S,
+    addr: SocketAddr,
+    outer_tx: std::sync::mpsc::Sender<PollnetMessage>,
+) -> anyhow::Result<()>
+where
+    S: AsyncRead + AsyncWrite + Unpin,
+{
+    // rx_to_sock: tokio::sync::mpsc::Receiver<SocketMessage>
+    // tx_from_sock: std::sync::mpsc::Sender<SocketMessage>
+    let (tx_to_sock, rx_to_sock) = tokio::sync::mpsc::channel(100);
+    let (tx_from_sock, rx_from_sock) = std::sync::mpsc::channel();
+
+    let sendres = outer_tx.send(PollnetMessage::NewClient(ClientConn {
+        tx: tx_to_sock,
+        rx: rx_from_sock,
+        id: addr.to_string(),
+    }));
+    if sendres.is_err() {
+        stream.shutdown().await.unwrap_or_default();
+        return Err(anyhow!("Fuck"));
+    }
+
+    match accept_async(stream).await {
+        Ok(ws_stream) => {
+            websocket_poll_loop(ws_stream, tx_from_sock, rx_to_sock).await;
+        }
+        Err(err) => {
+            error!("connection error: {}", err);
+            eh(tx_from_sock.send(PollnetMessage::Error(err.to_string())))?;
+        }
+    }
+    Ok(())
 }
 
 async fn accept_ws<S>(
@@ -81,28 +130,8 @@ async fn accept_ws<S>(
 ) where
     S: AsyncRead + AsyncWrite + Unpin,
 {
-    //rx_to_sock: tokio::sync::mpsc::Receiver<SocketMessage>, tx_from_sock: std::sync::mpsc::Sender<SocketMessage>) {
-    let (tx_to_sock, rx_to_sock) = tokio::sync::mpsc::channel(100);
-    let (tx_from_sock, rx_from_sock) = std::sync::mpsc::channel();
-
-    outer_tx
-        .send(PollnetMessage::NewClient(ClientConn {
-            tx: tx_to_sock,
-            rx: rx_from_sock,
-            id: addr.to_string(), //"BLURGH".to_string(),
-        }))
-        .expect("this shouldn't ever break?");
-
-    match accept_async(stream).await {
-        Ok(ws_stream) => {
-            websocket_poll_loop(ws_stream, tx_from_sock, rx_to_sock).await;
-        }
-        Err(err) => {
-            error!("connection error: {}", err);
-            tx_from_sock
-                .send(PollnetMessage::Error(err.to_string()))
-                .expect("TX error on connection error");
-        }
+    if let Err(e) = accept_ws_inner(stream, addr, outer_tx).await {
+        error!("Channel issue: {:?}", e);
     }
 }
 
@@ -133,7 +162,7 @@ impl PollnetContext {
                     error!("WS client connection error: {}", err);
                     tx_from_sock
                         .send(PollnetMessage::Error(err.to_string()))
-                        .expect("TX error on connection error");
+                        .unwrap_or_default();
                 }
             }
         });
@@ -162,7 +191,10 @@ impl PollnetContext {
                 }
             };
             info!("WS server waiting for connections on {}", addr);
-            tx_from_sock.send(PollnetMessage::Connect).expect("oh boy");                    
+            if tx_from_sock.send(PollnetMessage::Connect).is_err() {
+                error!("Channel died before entering serve loop!");
+                return;
+            }
             loop {
                 tokio::select! {
                     from_c_message = rx_to_sock.recv() => {
@@ -177,7 +209,7 @@ impl PollnetContext {
                                 tokio::spawn(accept_ws(tcp_stream, addr, tx_from_sock.clone()));
                             },
                             Err(msg) => {
-                                tx_from_sock.send(PollnetMessage::Error(msg.to_string())).expect("TX error on socket error");
+                                tx_from_sock.send(PollnetMessage::Error(msg.to_string())).unwrap_or_default();
                                 break;
                             }
                         }
